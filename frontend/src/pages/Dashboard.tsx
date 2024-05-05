9ee6aeee98874ae7ffdc8e4ce3e6cf49e9b11991
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-05-05 01:00:51 +0000

    Implementing modal

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -96,7 +96,7 @@ const Dashboard = () => {
     }
   return (
     <RootLayout>
-    <div className="mt-10 flex flex-wrap justify-center space-x-5 space-y-5">{
+    <div className="mt-10 flex flex-wrap justify-center space-x-5 gap-y-5">{
         states.events.map((card: any) => {
             return( 
             <Card isAttendee={isAttendee(card.attendants)} btnTitle="Join" key={card.id} title={card.name} date={card.date} description={card.description} btnClick={() => addToAttendees(card)}/>
